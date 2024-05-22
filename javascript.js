4f6d1db358edc9137531ce38021d372eaa9628ca
hibibiku <44918203+hibibiku@users.noreply.github.com>
2024-05-22 15:07:40 +0800

    removed js

--- javascript.js
+++ javascript.js
@@ -1,20 +1,22 @@
-$(document).ready(function() {
-    // When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
+// $(document).ready(function() {
+//     // When the user scrolls the page, execute myFunction
+// window.onscroll = function() {myFunction()};
 
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-    // Get the navbar
-const navbar = document.getElementById("navbar-div");
+// // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+// function myFunction() {
+//     // Get the navbar
+// const navbar = document.getElementById("navbar-div");
+// const content = document.getElementById('')
 
-// Get the offset position of the navbar
-const sticky = navbar.offsetTop;
+// // Get the offset position of the navbar
+// const sticky = navbar.offsetTop;
 
-  if (window.scrollY > sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
+//   if (window.scrollY > sticky) {
+//     navbar.classList.add("sticky")
+
+//   } else {
+//     navbar.classList.remove("sticky");
+//   }
   
-}
- });
+// }
+//  });
