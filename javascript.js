47a4ef7eddcaa5fa8b4f8d8a63446e8a84e1d58f
hibibiku <44918203+hibibiku@users.noreply.github.com>
2024-05-22 17:54:32 +0800

    added animations for cards

--- javascript.js
+++ javascript.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
   let overlay = document.getElementsByClassName("overlay")[0];
   let loading = document.getElementsByClassName("loading")[0];
 
-  const load = document.getElementById("loading");
+  const load = document.getElementById("dot");
   setInterval(function() {load.innerHTML += " . "}, 500);
   
   
@@ -18,12 +18,21 @@ $(document).ready(function() {
     // When the user scrolls the page, execute myFunction
 window.onscroll = function() {myFunction()};
 
+
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
-  const para = document.getElementsByClassName("para");
-  for (let i = 0; i < para.length; i++) {
-    para[i].style.backgroundColor = "red";
+
+  var content = document.getElementById("content-div")
+
+  var top = $(document).scrollTop();
+
+  if (window.screenY < top) {
+    content.style.backgroundColor = "var(--melon)";
+  }
+  else {
+    content.style.backgroundColor = "#ffffff";
   }
 
+  
 }
  });
