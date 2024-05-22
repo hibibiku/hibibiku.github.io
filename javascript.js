b5a10636faf9d6467428c994c834f53fb6ffae4d
hibibiku <44918203+hibibiku@users.noreply.github.com>
2024-05-22 16:12:16 +0800

    added loading

--- javascript.js
+++ javascript.js
@@ -1,22 +1,29 @@
-// $(document).ready(function() {
-//     // When the user scrolls the page, execute myFunction
-// window.onscroll = function() {myFunction()};
+document.addEventListener("DOMContentLoaded", () => {
+  let overlay = document.getElementsByClassName("overlay")[0];
+  let loading = document.getElementsByClassName("loading")[0];
 
-// // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-// function myFunction() {
-//     // Get the navbar
-// const navbar = document.getElementById("navbar-div");
-// const content = document.getElementById('')
-
-// // Get the offset position of the navbar
-// const sticky = navbar.offsetTop;
-
-//   if (window.scrollY > sticky) {
-//     navbar.classList.add("sticky")
-
-//   } else {
-//     navbar.classList.remove("sticky");
-//   }
+  const load = document.getElementById("loading");
+  setInterval(function() {load.innerHTML += " . "}, 500);
   
-// }
-//  });
+  
+
+  setTimeout(() => {
+    loading.remove();
+    overlay.classList.add("loaded");
+  }, 3000);
+});
+
+$(document).ready(function() {
+
+    // When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  const para = document.getElementsByClassName("para");
+  for (let i = 0; i < para.length; i++) {
+    para[i].style.backgroundColor = "red";
+  }
+
+}
+ });
