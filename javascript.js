86bd03ba82c62394217e349235c788665b7bdb12
hibibiku <44918203+hibibiku@users.noreply.github.com>
2024-05-22 14:54:41 +0800

    changes made to button animation, added hero text

--- javascript.js
+++ javascript.js
@@ -5,20 +5,16 @@ window.onscroll = function() {myFunction()};
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
     // Get the navbar
-var navbar = document.getElementById("navbar-div");
-
-var content = document.getElementById("content-div");
+const navbar = document.getElementById("navbar-div");
 
 // Get the offset position of the navbar
-var sticky = navbar.offsetTop;
+const sticky = navbar.offsetTop;
 
   if (window.scrollY > sticky) {
     navbar.classList.add("sticky")
-    
   } else {
     navbar.classList.remove("sticky");
   }
-
-    
+  
 }
  });
